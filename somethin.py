576de0bc9e213a2973b56b007f334a55847dcf61
Hatake <75223711+LiLBoyHatake@users.noreply.github.com>
2021-01-05 12:12:30 -0500

    Re-made code. Not working right now bug fixing later

--- somethin.py
+++ somethin.py
@@ -1,102 +1,108 @@
-import time
+from tkinter import *
 
-#Just setting things up to make it a application
-import tkinter as tk
+expression = ""
 
-
-start = input('Want to use the calculator? (Y for yes) - ')
-
-
-#Application base
-
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.pack()
-        self.create_widgets()
+def touch(num):
+    global expression
 
 
-    def create_widgets(self):
-        #create first button
-        self.simp_one = tk.Button(self)
-        self.simp_one['text'] = ' 1 ' #Am I suppossed to make it a command?
-        self.simp_one.pack(side='top')
+    expression = expression + str(num)
 
-        #Create second button
-        self.simp_two = tk.Button(self)
-        self.simp_two['text'] = ' 2 '
-        self.simp_two.pack(side='top')
+    equation.set(expression)
 
-        #Third button
-        self.simp_third = tk.Button(self)
-        self.simp_third['text'] = ' 3 '
-        self.simp_third.pack(side='top')
+def equalspress():
+    try:
+        global expression
+        total = str(eval(expression))
+        equation.set(total)
 
-        #Fourth button
-        self.simp_four = tk.Button(self)
-        self.simp_four['text'] = ' 4 '
-        self.simp_four.pack(side='top')
+        expression = ""
 
-        #Fifth button
-        self.simp_five = tk.Button(self)
-        self.simp_five['text'] = ' 5 '
-        self.simp_five.pack(side='top')
-
-        #Sixth button
-        self.simp_six = tk.Button(self)
-        self.simp_six['text'] = ' 6 '
-        self.simp_six.pack(side='top')
-
-        #Seventh button
-        self.simp_sev = tk.Button(self)
-        self.simp_sev['text'] = ' 7 '
-        self.simp_sev.pack(side='top')
-
-        #Button eight
-        self.simp_eight = tk.Button(self)
-        self.simp_eight['text'] = ' 8 '
-        self.simp_eight.pack(side='top')
-        
-        #Ninth button
-        self.simp_ninth = tk.Button(self)
-        self.simp_ninth['text'] = ' 9 '
-        self.simp_ninth.pack(side='right')
+    except:
+        equation.set('Somethings wrong')
+        expression = ""
 
 
-        #Create the zero button
-        self.simp_zed = tk.Button(self)
-        self.simp_zed['text'] = ' 0 '
-        self.simp_zed.pack(side='bottom')
+def clear():
+    global expression
+    expression = ""
+    equation.set("")
 
 
-#Function
-if start == 'Y':
-    #Define everything
-    x1 = int(input('Please enter your first number - '))
-    time.sleep(.5)
-    x2 = int(input('Please enter your second number - '))
-    time.sleep(.5)
-    oper = input('Please select an operator, (+, -, /) - ')
+if __name__ == '__main__':
+    gui = Tk()
+    gui.configure(background='purple')
+    gui.title('Hatake\'s Calculator')
+    gui.geometry("270x150")
 
-    #Calculate
-    if oper == '+':
-        print(x1 + x2)
-    elif oper == '-':
-        print(x1 - x2)
-    elif oper == '/':
-        print(x1 / x2)
+    equation = StringVar()
+    expression_field = Entry(gui, textvariable=equation)
+    expression_field.grid(columnspan=4, ipadx=70)
+    equation.set('Enter your equation')
+
+
+    #Buttons
+    button1 = Button(gui, text = ' 1 ', fg='black', bg='red', 
+        command=lambda: touch(1), height=1, width=7)
+    button1.grid(row=2, column=0)
+    button2 = Button(gui, text=' 2 ', fg='black', bg='red', 
+                     command=lambda: touch(2), height=1, width=7) 
+    button2.grid(row=2, column=1) 
+  
+    button3 = Button(gui, text=' 3 ', fg='black', bg='red', 
+                     command=lambda: touch(3), height=1, width=7) 
+    button3.grid(row=2, column=2) 
+  
+    button4 = Button(gui, text=' 4 ', fg='black', bg='red', 
+                     command=lambda: touch(4), height=1, width=7) 
+    button4.grid(row=3, column=0) 
+  
+    button5 = Button(gui, text=' 5 ', fg='black', bg='red', 
+                     command=lambda: touch(5), height=1, width=7) 
+    button5.grid(row=3, column=1) 
+  
+    button6 = Button(gui, text=' 6 ', fg='black', bg='red', 
+                     command=lambda: touch(6), height=1, width=7) 
+    button6.grid(row=3, column=2) 
+  
+    button7 = Button(gui, text=' 7 ', fg='black', bg='red', 
+                     command=lambda: touch(7), height=1, width=7) 
+    button7.grid(row=4, column=0) 
+  
+    button8 = Button(gui, text=' 8 ', fg='black', bg='red', 
+                     command=lambda: touch(8), height=1, width=7) 
+    button8.grid(row=4, column=1) 
+  
+    button9 = Button(gui, text=' 9 ', fg='black', bg='red', 
+                     command=lambda: touch(9), height=1, width=7) 
+    button9.grid(row=4, column=2) 
+  
+    button0 = Button(gui, text=' 0 ', fg='black', bg='red', 
+                     command=lambda: touch(0), height=1, width=7) 
+    button0.grid(row=5, column=0)
+
+    plus = Button(gui, text = ' + ', fg='black', bg='red', 
+        command=lambda: touch("+"), height=1, width=7)
+    plus.grid(row=2, column=3)
+
+    minus = Button(gui, text=' - ', fg='black', bg='red', 
+        command=lambda: touch("-"), height=1, width=7)
+    minus.grid(row=3, column=3)
+
+    multi = Button(gui, text = 'X', fg='black', bg='red', 
+        command=lambda: touch('*'), height=1, width=7)
+    multi.grid(row=4, column=3)
+
+    division = Button(gui, text = ' / ', fg='black', bg='red',
+        command=lambda: touch('/'), height=1, width=7)
+    division.grid(row=5, column=3)
+
+    clear = Button(gui, text = ' CLEAR ', fg='black', bg='red', 
+        command=clear, height=1, width=7)
+    clear.grid(row=5, column='1')
     
-else:
-    print("Error please make sure everything is correct")
-    pass
-time.sleep(1.8)
-print('Thank you for using LiLBoyHatake\'s calculator! It is much appreciated. Please visit my website lilboyhatake.me')
-time.sleep(2)
-print('if you would like to use the calculator again, please restart the program')
-time.sleep(3)
+    decimal = Button(gui, text = ' . ', fg='black', bg='red',  
+        command=lambda: touch('.'), height=1, width=7)
+    decimal.grid(row=6, column=0)
 
-
-
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+    gui.mainloop()
